a2bd3242afc01f91aba3cc9e95ded8384e209629
Neriya Malachi <neriamalachi@gmail.com>
2024-03-04 15:43:46 +0200

    update the next-config.mjs file

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"AHbxztS+k3q1xmPbFiLfB/Ni2z7ZFYSC2ehg351JBN4=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"egYxhBUgUc6WeqpxLPJHCEJ2nT4XIUmOE/OGzoPgIl8=\"}"
