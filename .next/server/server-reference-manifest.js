c2820529e983554a6c6fc461befc06c1ba077265
Neriya Malachi <neriamalachi@gmail.com>
2024-03-04 16:15:25 +0200

    .

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"egYxhBUgUc6WeqpxLPJHCEJ2nT4XIUmOE/OGzoPgIl8=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"A1tANjleHMOtbvYXRCEMuK9fTX/hqMNBRLq/o2izano=\"}"
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
+
 const nextConfig = {
-    
+    basePath: isProd ? '/QR-Card-Front-End' : '',
     output: 'export',
-    // distDir: 'dist',
+    distDir: 'dist',
     images: {
         unoptimized: true
     }
