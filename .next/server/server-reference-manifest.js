422e0e0b9fb5618a40042ca875ffa56c1b2e0567
Neriya Malachi <neriamalachi@gmail.com>
2024-03-05 13:16:39 +0200

    .

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"0Z+gCarV1oS/qqkNHLaBidNxPGjq8Tmq4iZdgCNwzYQ=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"uOPR/W0K6BKhgSpWsfXJ9DhmFFKikDiZnFqJ9PdHhiE=\"}"
